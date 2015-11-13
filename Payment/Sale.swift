2b70c250c57544663b2b8c677343fb3729c15c12
Fregy <fregy@hotmail.com>
2015-11-13 11:16:13 -0500

    New File for Sales - Alfredo

---
+++ Payment/Sale.swift
@@ -0,0 +1,15 @@
+//
+//  Sale.swift
+//  Payment
+//
+//  Created by MCS on 11/13/15.
+//  Copyright © 2015 Mobile Consulting Solutions. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Sale: NSManagedObject {
+    var id = 0
+    var quantity = 0
+}
