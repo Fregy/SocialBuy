c3bc3a501c518d9248507f3fc0a535d02ec4dd92
Fregy <fregy@hotmail.com>
2015-11-14 13:43:22 -0500

    fix warning of Products

--- Payment/DataBase.swift
+++ Payment/DataBase.swift
@@ -11,27 +11,27 @@ import CoreData
 
 class DataBase: NSManagedObject {
     
-    func insertItem(nameTable:String, myItem:Product){
-        
-        let auxProduct = NSEntityDescription.insertNewObjectForEntityForName(nameTable,
-            inManagedObjectContext: self.managedObjectContext!) as! Product
-        
-        auxProduct.id       = myItem.id
-        auxProduct.name     = myItem.name
-        auxProduct.desc     = myItem.desc
-        auxProduct.price    = myItem.price
-        auxProduct.discount = myItem.discount
-        auxProduct.quantity = myItem.quantity
-        
-        do {
-            
-            try self.managedObjectContext!.save()
-            
-        } catch {
-            print("Unable to Insert")
-            abort()
-        }
-    }
+//    func insertItem(nameTable:String, myItem:Product){
+//        
+//        let auxProduct = NSEntityDescription.insertNewObjectForEntityForName(nameTable,
+//            inManagedObjectContext: self.managedObjectContext!) as! Product
+//        
+//        auxProduct.id       = myItem.id
+//        auxProduct.name     = myItem.name
+//        auxProduct.desc     = myItem.desc
+//        auxProduct.price    = myItem.price
+//        auxProduct.discount = myItem.discount
+//        auxProduct.quantity = myItem.quantity
+//        
+//        do {
+//            
+//            try self.managedObjectContext!.save()
+//            
+//        } catch {
+//            print("Unable to Insert")
+//            abort()
+//        }
+//    }
     
     func insertSale(nameTable:String, myItem:Sale){
         
