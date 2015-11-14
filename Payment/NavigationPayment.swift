fef71b7989742c6c86a20a790b7c4d2496b45a7d
Fregy <fregy@hotmail.com>
2015-11-13 19:32:05 -0500

    some changes

--- Payment/NavigationPayment.swift
+++ Payment/NavigationPayment.swift
@@ -9,14 +9,28 @@
 import UIKit
 
 class NavigationPayment: UINavigationController {
-
+    
     var myArrayItems: [Product]!
     
-    // Segue Function
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        let vc          = segue.destinationViewController as! PaymentViewController
-    
-        vc.myArrayItems = myArrayItems
+        // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let vc          = segue.destinationViewController as! PaymentViewController
+        
+        vc.myArrayItems = myArrayItems
+
+    }
+    
 }
